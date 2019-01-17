bce9276e9d50e77d29bf0c1e63a3b0a3a9090e03
andriy.nikiforov <andriy.nikiforov.general@gmail.com>
2019-01-17 17:30:45 +0200

    Fix path

--- src/generator/index.js
+++ src/generator/index.js
@@ -1,19 +1,20 @@
 'use strict';
 
 const unzip = require('adm-zip');
+const { resolve } = require('path');
 
 const appiumExt = (path) => {
-  const zip = new unzip('./assets/appiumwdblui.zip');
+  const zip = new unzip(resolve('./assets/appiumwdblui.zip'));
   zip.extractAllTo(path, true);
 };
 
 const seleniumExt = (path) => {
-  const zip = new unzip('./assets/selblui.zip');
+  const zip = new unzip(resolve('./assets/selblui.zip'));
   zip.extractAllTo(path, true);
 };
 
 const webdriverExt = (path) => {
-  const zip = new unzip('./assets/selwdioblui.zip');
+  const zip = new unzip(resolve('./assets/selwdioblui.zip'));
   zip.extractAllTo(path, true);
 };
 
