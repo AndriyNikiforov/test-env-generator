2773acda7fad3635dee1a57b65ca46e5607c2b09
andriy.nikiforov <andriy.nikiforov.general@gmail.com>
2019-01-25 16:33:23 +0200

    Speed up archive creating. Speed up archive executing.

--- src/api/zip.js
+++ src/api/zip.js
@@ -1,7 +1,6 @@
 /* eslint-disable class-methods-use-this */
 const fs = require('fs-extra');
 const Unzip = require('adm-zip');
-const zipFolder = require('zip-folder');
 const { resolve } = require('path');
 const { error, log } = require('console');
 
@@ -11,25 +10,25 @@ const { error, log } = require('console');
 class Zip {
   /**
    * @description Execute zip archive
-   * @param {String} pathArch
+   * @param {String} pathToArch
    * @param {String} pathToExt
    */
-  exectArch(pathArch, pathToExt) {
-    const zip = new Unzip(resolve(__dirname, pathArch));
-    zip.extractAllTo(pathToExt, true);
+  async exectArch(pathToArch, pathToExt) {
+    const zip = new Unzip(resolve(__dirname, pathToArch));
+    await zip.extractAllTo(pathToExt, true);
   }
 
   /**
    * @description Make zip archive
    * @param {String} name
    */
-  makeArch(name) {
-    zipFolder('./tmp', `./${name}.zip`, (err) => {
-      if (err) log('Error: ', err);
-      fs.remove('./tmp', (bug) => {
-        if (bug) error(bug);
-        log('EXCELLENT');
-      });
+  async makeArch(name) {
+    const zip = new Unzip();
+    await zip.addLocalFolder('./tmp', './');
+    await zip.writeZip(`${name}.zip`);
+    await fs.remove('./tmp', (bug) => {
+      if (bug) error(bug);
+      log('EXCELLENT');
     });
   }
 }
