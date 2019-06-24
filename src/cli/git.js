6019043400a9505a9b55db43bf6f29809a49b89a
AndriyNikiforov <andriy.nikiforov.general@gmail.com>
2019-06-24 11:43:32 +0300

    Refactoring.

--- src/cli/git.js
+++ src/cli/git.js
@@ -1,6 +1,20 @@
+const { spawn } = require('child_process');
+const { error } = require('console');
 const { prompt } = require('inquirer');
-const cloneSkeleton = require('../api/git');
+
+const { makeArch } = require('../api/zip');
 const { gitQuestion } = require('./config/');
+
+/**
+ * Start process cloning repository from github
+ * @param {String} address
+ * @param {String} folderName
+ */
+const cloneSkeleton = (address, folderName) => {
+  const process = spawn('git', ['clone', address]);
+  process.on('close', () => makeArch('skeleton', folderName));
+  process.on('error', err => error(err));
+};
 
 const address = {
   wdio: 'https://github.com/AndriyNikiforov/selwdioblui.git',
